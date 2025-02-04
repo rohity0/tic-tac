440e964393f10cb29d623b860c04c9f87370533e
rohity0 <yadavry09@gmail.com>
2025-02-04 21:15:00 +0530

    hotjar

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
+import { injectContentsquareScript } from '@contentsquare/tag-sdk';
 
+injectContentsquareScript({
+  siteId: "5291598",
+  async: true, // Optional: Set to false to wait for script execution until after document parsing.
+  defer: false // Optional: Set to true to defer script execution after document parsing.
+});
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
